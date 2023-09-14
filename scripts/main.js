be1de4eb9b8fa8a8186650109886786badc3bfcf
zklin99 <70021235+zklin99@users.noreply.github.com>
2023-09-14 14:58:37 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+// let myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName){
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    }
+}
+
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+}
+
+myButton.onclick = function () {
+setUserName();
+};
